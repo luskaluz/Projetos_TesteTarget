528319131095bdf0a3fad68bdde6361c262118a8
luskaluz <109252997+luskaluz@users.noreply.github.com>
2024-11-06 12:53:01 -0300

    Add files via upload

---
+++ Target1_soma/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    //1
+    private static int Soma()
+    {
+        int indice = 13, soma = 0, k = 0;
+
+        while (k < indice)
+        {
+            k = k + 1;
+            soma = soma + k;
+        }
+
+        return soma;
+
+        //Resultado = 91
+    }
+
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Soma: " + Soma());
+    }
+}
---
+++ Target2_fibonacci/Program.cs
@@ -0,0 +1,36 @@
+﻿class Program
+{
+    private static bool EstaFibo(int n)
+    {
+        int a = 0, b = 1;
+
+        if (n == a || n == b)
+        {
+            return true;
+        }
+        int fibo = a + b;
+        while (fibo <= n)
+        {
+            if (fibo == n)
+                return true;
+
+            a = b;
+            b = fibo;
+            fibo = a + b;
+        }
+        return false;
+    }
+
+    private static void Main(string[] args)
+    {
+        int n = 5;
+        if (EstaFibo(n))
+        {
+            Console.WriteLine("2. {0} está na sequência", n);
+        }
+        else
+        {
+            Console.WriteLine("2. {0} não está na sequência", n);
+        }
+    }
+}
