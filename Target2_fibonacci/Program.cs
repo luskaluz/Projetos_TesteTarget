b70c915317a27144516b32f6c79b6214d67b2526
luskaluz <109252997+luskaluz@users.noreply.github.com>
2024-11-06 13:03:34 -0300

    Add files via upload

---
+++ Target2_fibonacci/Program.cs
@@ -0,0 +1,45 @@
+﻿class Program
+{
+    private static bool EstaFibo(int n)
+    {
+        int a = 0, b = 1;
+
+        if (n == a || n == b)
+        {
+            return true;
+        }
+        int fibo = a + b;
+        while (fibo <= n)
+        {
+            if (fibo == n)
+                return true;
+
+            a = b;
+            b = fibo;
+            fibo = a + b;
+        }
+        return false;
+    }
+
+    private static void Main(string[] args)
+    {
+        int n = 5;
+        int error = 4;
+        if (EstaFibo(n))
+        {
+            Console.WriteLine("2. {0} está na sequência", n);
+        }
+        else
+        {
+            Console.WriteLine("2. {0} não está na sequência", n);
+        }
+        if (EstaFibo(error))
+        {
+            Console.WriteLine("2. {0} está na sequência", error);
+        }
+        else
+        {
+            Console.WriteLine("2. {0} não está na sequência", error);
+        }
+    }
+}
